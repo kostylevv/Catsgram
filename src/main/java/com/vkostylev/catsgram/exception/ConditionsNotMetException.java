60d6ae973199c6c61d9d134871f63d5288600545
Vladislav Kostylev <kostylev.v@gmail.com>
2024-05-20 11:27:12 +0300

    feat: spring boot

---
+++ src/main/java/com/vkostylev/catsgram/exception/ConditionsNotMetException.java
@@ -0,0 +1,7 @@
+package com.vkostylev.catsgram.exception;
+
+public class ConditionsNotMetException extends RuntimeException {
+    public ConditionsNotMetException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/vkostylev/catsgram/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.vkostylev.catsgram.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
